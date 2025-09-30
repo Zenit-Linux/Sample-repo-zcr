b32e78cd26627f5f8a84c0879866d4131729cdc8
VoidArc Studio <chudybyczek2137@gmail.com>
2025-09-30 09:05:41 +0200

    Update unpack.sh

--- zcr-build-files/unpack.sh
+++ zcr-build-files/unpack.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo mv /usr/lib/zcr/sample-zcr-repo/sample-zcr-repo.desktop /usr/share/applications/
 sudo mv /usr/lib/zcr/sample-zcr-repo/sample-zcr-repo /usr/bin/
+sudo chmod a+x /usr/bin/sample-zcr-repo
+sudo chmod a+x /usr/lib/zcr/Sample-zcr-repo/sample-program.py
